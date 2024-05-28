0fcc13a668911a2e112366f85573852bb33f4077
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-05-28 22:23:45 +0500

    edit profile work flow compelet

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,7 +9,7 @@ import {
   query,
   doc,
   deleteDoc,
-  updateDoc
+  updateDoc,
 } from "../../firebase/firebaseConfig";
 import { serverTimestamp } from "firebase/database";
 
@@ -17,6 +17,10 @@ const Profile = () => {
   const [post, setPost] = useState("");
   const [allPosts, setAllPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [editPost, setEditPost] = useState({
+    editId: null,
+    editText: "",
+  });
 
   useEffect(() => {
     const getData = async () => {
@@ -38,7 +42,7 @@ const Profile = () => {
       unsubscribe = onSnapshot(q, (querySnapshot) => {
         const posts = [];
         querySnapshot.forEach((doc) => {
-          posts.push({id:doc.id, ...doc.data()});
+          posts.push({ id: doc.id, ...doc.data() });
         });
 
         setAllPosts(posts);
@@ -63,8 +67,8 @@ const Profile = () => {
         // createdOn: new Date().getTime(),
         createdOn: serverTimestamp(),
       });
-      setIsLoading(false);
       e.target.reset();
+      setIsLoading(false);
       console.log("Document written with ID: ", createPost.id);
     } catch (e) {
       console.error("Error adding document: ", e);
@@ -74,10 +78,23 @@ const Profile = () => {
 
   const deleteHandler = async (postId) => {
     // console.log("postId->", postId);
-    await deleteDoc(doc(db,"posts", postId));
-  }
+    await deleteDoc(doc(db, "posts", postId));
+  };
 
-
+  const updatePostHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await updateDoc(doc(db, "posts", editPost.editId), {
+        text: editPost.editText,
+      });
+      setEditPost({
+        editId: null,
+        editText: "",
+      });
+    } catch (error) {
+      console.log("error->", error.message);
+    }
+  };
 
   return (
     <div>
@@ -98,21 +115,55 @@ const Profile = () => {
       </form>
 
       <div className="flex flex-wrap justify-center gap-x-20 gap-y-4">
-        {/* {isLoading && (
-          <div className="text-center text-xl ">Data Loading....</div>
-        )} */}
         {allPosts.map((eachPost, idx) => {
-          // console.log("eachPost->", eachPost);
           return (
-            <div key={idx} className=" w-1/4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white ">
+            <div
+              key={idx}
+              className=" w-1/4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white "
+            >
               <p className="mb-3 text-center text-xl py-10 text-gray-700 dark:text-gray-700">
-                {eachPost?.text}
+                {eachPost.id === editPost.editId ? (
+                  <textarea
+                    type="text"
+                    value={editPost?.editText}
+                    placeholder="Enter updated value.."
+                    className="bg-[#f1faee]"
+                    onChange={(e) => {
+                      setEditPost({
+                        ...editPost,
+                        editText: e.target.value,
+                      });
+                    }}
+                  />
+                ) : (
+                  eachPost?.text
+                )}
               </p>
               <div className="flex gap-3 justify-center">
-                <button className="px-5 py-2 font-medium text-center text-white bg-[#] rounded-lg hover:bg-[#3cadc4] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#00b4d8] dark:hover:bg-[#269bb3] dark:focus:ring-blue-800">
-                  Edit
-                </button>
-                <button onClick={() => deleteHandler(eachPost?.id)} className="px-3 py-2 font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-[#ef233c] dark:hover:bg-red-700 dark:focus:ring-red-800">
+                {editPost.editId === eachPost?.id ? (
+                  <button
+                    className="px-5 py-2 font-medium text-center text-white bg-[#] rounded-lg hover:bg-[#3cadc4] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#00b4d8] dark:hover:bg-[#269bb3] dark:focus:ring-blue-800"
+                    onClick={updatePostHandler}
+                  >
+                    Update
+                  </button>
+                ) : (
+                  <button
+                    className="px-5 py-2 font-medium text-center text-white bg-[#] rounded-lg hover:bg-[#3cadc4] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#00b4d8] dark:hover:bg-[#269bb3] dark:focus:ring-blue-800"
+                    onClick={() => {
+                      setEditPost({
+                        editId: eachPost?.id,
+                        editText: eachPost?.text,
+                      });
+                    }}
+                  >
+                    Edit
+                  </button>
+                )}
+                <button
+                  onClick={() => deleteHandler(eachPost?.id)}
+                  className="px-3 py-2 font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-[#ef233c] dark:hover:bg-red-700 dark:focus:ring-red-800"
+                >
                   Delete
                 </button>
               </div>
