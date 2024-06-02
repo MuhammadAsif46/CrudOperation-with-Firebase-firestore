df0adcb85a1b1a0c5bfac12ed51686052d97a950
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-02 10:34:32 +0500

    added antd upload

--- src/components/UpdateProfile/UpdateProfile.jsx
+++ src/components/UpdateProfile/UpdateProfile.jsx
@@ -54,7 +54,7 @@ const UpdateProfile = () => {
     }
   };
 
-  console.log(imageUpload);
+//   console.log(imageUpload);
 
   return (
     <div className="bg-white p-5 mt-10 mx-auto w-1/3 rounded-md shadow-lg">
@@ -64,48 +64,52 @@ const UpdateProfile = () => {
           type="file"
           id="file"
           onChange={(e) => {
-            const base64Url = URL.createObjectURL(e.target.files[0])
+            const base64Url = URL.createObjectURL(e.target.files[0]);
             setImageUpload(base64Url);
           }}
         />
 
-        {imageUpload && (
-          <img
-            className=""
-            src={imageUpload}
-            alt="image"
-            width={100}
-            height={100}
-          />
+        {imageUpload ? (
+          <div>
+            <img
+              className=""
+              src={imageUpload}
+              alt="image"
+              width={150}
+              height="100%"
+            />
+          </div>
+        ) : (
+          <div className="border h-52 w-52 bg-purple-50 flex justify-center items-center">
+            <label htmlFor="file">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={100}
+                height={100}
+                fill="currentColor"
+                //   class="bi bi-person-fill-add"
+                className="cursor-pointer"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"
+                  fill="#ccc"
+                />
+                <path
+                  d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"
+                  fill="#ccc"
+                />
+              </svg>
+            </label>
+          </div>
         )}
-        <div className="border h-52 w-52 mx-auto bg-purple-50 flex justify-center items-center">
-          <label htmlFor="file">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={100}
-              height={100}
-              fill="currentColor"
-              //   class="bi bi-person-fill-add"
-              className="cursor-pointer"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"
-                fill="#ccc"
-              />
-              <path
-                d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"
-                fill="#ccc"
-              />
-            </svg>
-          </label>
-        </div>
       </div>
       <div className="mt-5">
         <input
           id="userName"
           name="userName"
           type="userName"
+          value={userName}
           autoComplete="userName"
           placeholder="Username"
           required
